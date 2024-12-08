0b67ea18827503c712e5b0d1bd5d6dd9ed0b697a
James Ulaszek <jaminthoseconcerts@gmail.com>
2024-12-07 19:28:19 -0500

    day3 p2

--- 2024/Day3.java
+++ 2024/Day3.java
@@ -7,20 +7,36 @@ public class Day3 {
         try (FileReader in = new FileReader("/Users/ulaszek/Projects/AdventOfCode/2024/input/day3.txt")) {
             BufferedReader br = new BufferedReader(in);
             String st;
-            int sum =  0;
+            long sum =  0;
+            boolean doDo = true;
 
             while ((st = br.readLine()) != null) {
-                while (st.contains("mul(")) {
-                    int index = st.indexOf("mul(");
-                    st = st.substring(index+4);
-                    // System.out.println(st + "\n");
-                    // int comma = st.indexOf(',');
-                    // System.out.println(st.substring(0, comma));
+                while (st.contains("do()") || st.contains("don't()")) {
+                    String temp;
+                    int doIndex = 0, dontIndex = 0;
 
-                    String result = validMul(st);
+                    if (doDo) {
+                        dontIndex = st.indexOf("don't()");
 
-                    if (result.chars().allMatch(Character::isDigit)) {
-                        sum += Integer.parseInt(result);
+                        if (dontIndex == -1) {
+                            sum += calculateMulOperations(st);
+                            st = "";
+                        } else {
+                            temp = st.substring(0, dontIndex);
+                            sum += calculateMulOperations(temp);
+
+                            st = st.substring(dontIndex); // get rid of the checkedString
+                            doDo = false;
+                        }
+                    } else {
+                        doIndex = st.indexOf("do()");
+
+                        if (doIndex == -1) {
+                            st = "";
+                        } else {
+                            st = st.substring(doIndex);
+                            doDo = true;
+                        }
                     }
                 }
             }
@@ -33,16 +49,34 @@ public class Day3 {
         }
     }
 
+    private static int calculateMulOperations(String st) {
+        int sum = 0;
+        
+        while (st.contains("mul(")) {
+            int index = st.indexOf("mul(");
+            st = st.substring(index+4);
+            // System.out.println(st + "\n");
+            // int comma = st.indexOf(',');
+            // System.out.println(st.substring(0, comma));
+            String result = validMul(st);
+            if (result.chars().allMatch(Character::isDigit)) {
+                sum += Integer.parseInt(result);
+            }
+        }
+        
+        return sum;
+    }
+
     private static String validMul(String mul) {
         int comma = mul.indexOf(",");
         int closeParan = mul.indexOf(")");
 
-        if (comma > 3) {
-            return "Comma too far";
+        if (comma > 3 || comma == -1) {
+            return "Comma too far or does not exist";
         }
 
-        if (closeParan > 7) {
-            return "CloseParan too far";
+        if (closeParan > 7 || closeParan == -1) {
+            return "CloseParan too far or does not exist";
         }
 
         String firstOperand = mul.substring(0, comma);
@@ -59,3 +93,22 @@ public class Day3 {
         return String.valueOf(Integer.parseInt(firstOperand) * Integer.parseInt(secondOperand));
     }
 }
+
+/* Part 1 
+ *  while ((st = br.readLine()) != null) {
+                while (st.contains("mul(")) {
+                    int index = st.indexOf("mul(");
+                    st = st.substring(index+4);
+                    // System.out.println(st + "\n");
+                    // int comma = st.indexOf(',');
+                    // System.out.println(st.substring(0, comma));
+
+                    String result = validMul(st);
+
+                    if (result.chars().allMatch(Character::isDigit)) {
+                        sum += Integer.parseInt(result);
+                    }
+                }
+            }
+
+ */
